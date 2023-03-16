055b150eee81891f7d75753ba9be997d29b05776
walo450 <hirwawalid43@gmail.com>
2023-03-16 10:08:45 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 1 success
+ */
+
+int main(void)
+{
+	putf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
