baef8cb5a979b66038c2b5710c482c9d82a733cb
walo450 <hirwawalid43@gmail.com>
2023-03-16 09:07:50 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is a function in c
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
