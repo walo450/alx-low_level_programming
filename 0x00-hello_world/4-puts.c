c2c0c1062b70936719471389c0ea5b9aa155c542
walo450 <hirwawalid43@gmail.com>
2023-03-16 09:34:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ *
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
